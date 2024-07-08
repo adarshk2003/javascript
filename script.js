fe921ad41976e764ce6541882f881cb582bc0d30
adarsh <reaemp4@gmail.com>
2024-07-08 11:33:07 +0530

    jss

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//global variable 
+
 var a;
 a = 10
 console.log(a);
@@ -6,3 +8,33 @@ console.log(a);
 var c =20;
 var d = 10;
 console.log("result is ",c+d*a);
+
+
+
+//string variable
+
+var s = "adarsh";
+console.log("type is", typeof s );
+
+//boolean type
+
+var p = true;
+console.log("type is :",typeof p);
+
+
+//number
+console.log("type is",typeof a)
+
+
+//block variable 
+{
+    let x;
+    x = 10;
+    let y;
+    y = 20;
+
+    console.log(x*y ,typeof x, typeof y)
+
+}
+// x and y is block elements so they can't access from outside 
+console.log(x*y);
